a2bd7de304616f2ff6f828ab7342aa26fc0f8c26
Rafał Korkosz <korkosz.rafal@gmail.com>
2015-01-29 00:52:03 +0100

    replace staticfiles with cloudinary

--- pm/settings.py
+++ pm/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -83,15 +82,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 CACHES = {
     'default': {
