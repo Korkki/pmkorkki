ee6741f95d044cefb8f4b14d7f2418a469e8be5d
Rafał Korkosz <korkosz.rafal@gmail.com>
2015-01-31 12:24:36 +0100

    Staticfiles config

--- pm/urls.py
+++ pm/urls.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 
@@ -7,4 +9,4 @@ urlpatterns = patterns('',
     url(r'^api/', include('todo.api.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'$', TemplateView.as_view(template_name='index.html'), name='index'),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
