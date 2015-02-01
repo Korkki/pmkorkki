c599265815682f529f134ca1024a2ffc6ffefbcc
Rafał Korkosz <korkosz.rafal@gmail.com>
2015-02-01 22:26:40 +0100

    revisiting whitenoise

--- pm/settings/base.py
+++ pm/settings/base.py
@@ -100,7 +100,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'dist'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'  # test
 
 SITE_ID = 1
 
